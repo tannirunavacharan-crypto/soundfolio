32c0cfb08c02c16f7503ad6beb28479014f721ab
Ak Bhuker <akbhuker642@gmail.com>
2026-06-29 10:38:20 +0530

    chore: add temporary /test-email diagnostic route to troubleshoot Render SMTP connection

--- server/routes/inquiryRoutes.js
+++ server/routes/inquiryRoutes.js
@@ -2,6 +2,25 @@ const express = require('express');
 const router = express.Router();
 const { createInquiry, getInquiries, deleteInquiry } = require('../controllers/inquiryController');
 const { protect } = require('../middleware/authMiddleware');
+const { sendInquiryEmail } = require('../utils/mailer');
+
+router.get('/test-email', async (req, res) => {
+  try {
+    const result = await sendInquiryEmail({
+      name: "Render Test Connection",
+      email: "akbhuker642@gmail.com",
+      phone: "1234567890",
+      projectType: "Diagnostic Test",
+      budget: "N/A",
+      timeline: "N/A",
+      description: "Diagnostic check to see if Render can connect to Gmail SMTP.",
+      platform: "Render Server"
+    });
+    return res.json(result);
+  } catch (err) {
+    return res.status(500).json({ success: false, error: err.message, stack: err.stack });
+  }
+});
 
 router.route('/')
   .post(createInquiry)
